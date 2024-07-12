aff23e65c40a753381938d5a5634af6cc2f281b6
Ananth Lakshman Kumar <ananthhockage@gmail.com>
2024-07-12 18:57:11 -0400

    clerk authentication added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+"use client";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated, useQuery } from "convex/react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello</div>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+      <Authenticated>
+        <UserButton />
+      </Authenticated>
     </main>
   );
 }
