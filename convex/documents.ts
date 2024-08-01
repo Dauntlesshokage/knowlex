f9f8bdcd6756b63ccdb95524e1488b555ed1e7a0
Ananth Lakshman Kumar <ananthhockage@gmail.com>
2024-08-01 17:34:02 -0400

    addes tokenIdentifier to database schema

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,17 +1,31 @@
 import { mutation, query } from "./_generated/server";
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
+
 export const createDocument = mutation({
   args: {
     title: v.string(),
   },
   async handler(ctx, args) {
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+    console.log(userId);
+    if (!userId) {
+      throw new ConvexError("User not authenticated");
+    }
     await ctx.db.insert("documents", {
       title: args.title,
+      tokenIdentifier: userId,
     });
   },
 });
 export const getDocuments = query({
   async handler(ctx) {
-    return await ctx.db.query("documents").collect();
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+    if (!userId) {
+      return [];
+    }
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", userId))
+      .collect();
   },
 });
